7469c24c1abd58d49f645a66dddf2c5d951acdac
8char <matteokrantz@gmail.com>
2023-04-18 20:17:06 +0200

    feat: add sample project

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,52 @@
-<h1 class="relative w-64 mx-auto px-4 py-8 md:px-0 text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
-    COMMING SOON!
-</h1>
+<script>
+	import ProjectImage from '$lib/assets/projects/demo-post.jpg';
+</script>
+
+<div class="md:max-w-5xl md:mx-auto py-8 space-y-10">
+	<div
+		class="flex flex-col rounded-md border-transparent border-2 hover:border-primary-300 transition duration-200 md:flex-row"
+	>
+		<div class="md:w-1/2">
+			<img class="w-full h-full rounded-l-md" src={ProjectImage} alt="Some text" />
+		</div>
+
+		<div class="md:w-1/2 bg-gray-800 p-5 rounded-r-md">
+			<div class="mb-4 space-x-2">
+				<span
+					class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-600 text-white"
+				>
+					CATEGORY
+				</span>
+			</div>
+
+			<a href="/posts/demo-post" class="block mt-2">
+				<p class="text-xl font-bold text-white">The Project Name</p>
+				<p class="mt-2 text-sm text-gray-300">
+					Not really done with this yet, this only serves as a placeholder for now... Not really
+					done with this yet, this only serves as a placeholder for now... Not really done with this
+					yet, this only serves as a placeholder for now... Not really done with this yet, this only
+					serves as a placeholder for now...
+				</p>
+
+				<p class="text-gray-500 text-xs mt-5">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="inline h-4 w-4"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+						/>
+					</svg>
+
+					Today - Now
+				</p>
+			</a>
+		</div>
+	</div>
+</div>
