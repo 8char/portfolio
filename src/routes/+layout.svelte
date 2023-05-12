950198608535eb4836bb98541c04563d1c5c6cb5
Matteo Krans Dusic <matteokrantz@gmail.com>
2023-05-12 11:49:50 +0200

    fix: add a meta description to the page to improve SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,10 @@
 
 <svelte:head>
 	<title>Atlas/8char</title>
+	<meta
+		name="description"
+		content="Welcome to my portfolio website! My name is Matteo Krans Dusic, aka Atlas, and I'm a freelance web developer with over 5 years of programming experience. Browse my projects and see my skills in action. If you're in need of a web developer, don't hesitate to reach out!"
+	/>
 </svelte:head>
 
 <div class="min-h-screen bg-gray-900">
