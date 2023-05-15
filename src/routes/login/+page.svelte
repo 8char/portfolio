ac699cd24b09cb12741127cabe614e7739e2fab7
Matteo Krans Dusic <matteokrantz@gmail.com>
2023-05-15 12:12:13 +0200

    feat: initial login prototype styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 	export let form: { message?: string };
+	import FontAwesomeIcon from 'svelte-fa';
 </script>
 
-<h2>Sign in</h2>
-<a href="/api/oauth?provider=github" class="button">Continue with Github</a>
-<p class="center">or</p>
-<form method="post" use:enhance>
-	<label for="username">username</label><br />
-	<input id="username" name="username" /><br />
-	<label for="password">password</label><br />
-	<input type="password" id="password" name="password" /><br />
-	<input type="submit" value="Continue" class="button" />
-</form>
-{#if form?.message}
-	<p class="error">{form.message || ''}</p>
-{/if}
-<a href="/signup" class="link">Create a new account</a>
+<div
+	class="rounded-lg bg-gray-900/40 backdrop-blur-xl ring-1 hover:bg-gray-800/50 transition shadow-xl flex flex-col items-center w-fit p-6 text-white mx-auto"
+>
+	<h2 class="text-2xl">Sign in</h2>
+	<a href="/api/oauth?provider=github" class="bg-gray-900 p-2 w-full rounded-md flex items-center">
+		<FontAwesomeIcon icon={faGithub} size="lg" class="mr-3 text-gray-400 group-hover:text-gray-300" />
+		<span class="mx-auto">Continue with Github</span>
+	</a>
+	<p class="center">━ or ━</p>
+	<form method="post" class="flex flex-col gap-2" use:enhance>
+		<label for="username" class="font-bold">USERNAME</label>
+		<input id="username" name="username" class="rounded-xl ring-1 hover:ring-lime-500/50 bg-gray-800 p-1" />
+		<label for="password">PASSWORD</label>
+		<input type="password" name="password" class="rounded-xl ring-1 hover:ring-lime-500/50 bg-gray-800 p-1" />
+		<input type="submit" value="Continue" class="ring-1 hover:ring-lime-500/50 rounded-xl bg-gray-900/40 hover:bg-lime-500/40 backdrop-blur-xl p-1 transition duration-300" />
+	</form>
+	{#if form?.message}
+		<p class="error">{form.message || ''}</p>
+	{/if}
+	<a href="/signup" class="italic">Create a new account</a>
+</div>
